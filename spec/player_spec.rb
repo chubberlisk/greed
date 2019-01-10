99ad187ed479b6108f3134697528fcc32b844fe7
Wen Ting Wang <chubberlisk@outlook.com>
2019-01-10 14:16:26 +0000

    test: add tests for #points

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,7 +1,18 @@
 require_relative '../lib/player'
 
 describe Player do
-  it 'can create a player' do
+  it 'creates a player' do
     expect(subject).not_to be_nil
+  end
+
+  describe '#points' do
+    it 'returns 0 when initialised' do
+      expect(subject.points).to eq(0)
+    end
+
+    it 'returns 100 when changed' do
+      subject.points = 100
+      expect(subject.points).to eq(100)
+    end
   end
 end
