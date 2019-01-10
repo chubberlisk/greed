2e5b5262da0235bd139e760affd3fe774ba15c4e
Wen Ting Wang <chubberlisk@outlook.com>
2019-01-10 19:36:18 +0000

    feat: create number of players based on user input

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,21 @@
+require_relative './player'
+
 class Game
   attr_reader :players
   attr_reader :end_game
 
   def initialize
+    @players = []
     @end_game = false
+  end
+
+  def main
+    puts '=' * 80
+    puts 'Greed'
+    puts '=' * 80
+    puts 'How many players?'
+    print '> '
+    num_of_players = STDIN.gets
+    num_of_players.to_i.times { @players.push(Player.new) }
   end
 end
