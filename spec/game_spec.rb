b15215f4c6ca369f2e30ae43d39d33380d00eceb
Wen Ting Wang <chubberlisk@outlook.com>
2019-01-10 16:28:49 +0000

    refactor: use subject and players not needed when initialised

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,30 +2,31 @@ require_relative '../lib/game'
 require_relative '../lib/player'
 
 describe Game do
-  let(:players) { [Player.new, Player.new] }
-  let(:game) { Game.new(players) }
-
   it 'creates a game' do
-    expect(game).not_to be_nil
+    expect(subject).not_to be_nil
   end
 
   describe '#players' do
+    let(:players) { [Player.new, Player.new] }
+
+    before { subject.instance_variable_set(:@players, players) }
+
     it 'returns an array' do
-      expect(game.players).to be_kind_of(Array)
+      expect(subject.players).to be_kind_of(Array)
     end
 
-    it 'returns an set of players' do
-      game.players.each { |player| expect(player).to be_kind_of(Player) }
+    it 'returns a set of players' do
+      subject.players.each { |player| expect(player).to be_kind_of(Player) }
     end
   end
 
   describe '#end_game' do
     it 'returns a boolean' do
-      expect(game.end_game).to be(true).or be(false)
+      expect(subject.end_game).to be(true).or be(false)
     end
 
     it 'returns false when initialised' do
-      expect(game.end_game).to be(false)
+      expect(subject.end_game).to be(false)
     end
   end
 end
