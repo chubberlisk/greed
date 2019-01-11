e65bfdf2745332c6a6dbf7ca18ef435a95299fd5
Wen Ting Wang <chubberlisk@outlook.com>
2019-01-11 17:10:16 +0000

    feat: add in_the_game attribute

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,14 @@
 class Player
-  attr_accessor :points
+  attr_reader :points
+  attr_reader :in_the_game
 
   def initialize
     @points = 0
+    @in_the_game = false
+  end
+
+  def points=(val)
+    @points = val
+    @in_the_game = true if @points >= 300
   end
 end
