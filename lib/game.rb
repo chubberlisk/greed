08836530883a0a2a4ce8922b6aa4c4cbdd5f2021
Wen Ting Wang <chubberlisk@outlook.com>
2019-01-15 17:10:02 +0000

    feat: use Turn class

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,4 @@
-require_relative './player'
-require_relative './dice_set'
+require_relative './turn'
 
 class Game
   attr_reader :players
@@ -15,7 +14,7 @@ class Game
   def main
     step_0_intro
     step_1_num_of_players until @players.size >= 2
-    step_2_turns
+    Turn.new(@players.first).main
   end
 
   def players_in_the_game
@@ -48,32 +47,5 @@ class Game
       puts "Please enter a number between 2 and 6.\n"
     end
     puts
-  end
-
-  def step_2_turns
-    # while !end_game
-      puts '=' * 6
-      puts "Current Turn: Player #{turn_player.number}"
-      puts "Player #{turn_player.number} is not in the game."
-      puts "You must get at least 300 points in this turn to get into the game.\n\n"
-      dice_set = DiceSet.new
-      dice_set.roll(5)
-      puts "You rolled #{dice_set.values} with a score of #{dice_set.score}."
-      if dice_set.score >= 300
-        turn_player.points += dice_set.score
-        puts "You are now in the game! You have #{turn_player.points} points."
-        num_of_dice = dice_set.num_of_non_scoring == 0 ? 5 : dice_set.num_of_non_scoring
-        ans = ask("Roll again with #{num_of_dice} dice? (y/n)")
-        if ans == 'y'
-          dice_set.roll(num_of_dice)
-          puts "You rolled #{dice_set.values} with a score of #{dice_set.score}."
-          turn_player.points += dice_set.score
-          puts "You have #{turn_player.points} points."
-        end
-      else
-        puts 'Unlucky, you\'re still not in the game.'
-      end
-      puts '=' * 6
-    # end
   end
 end
