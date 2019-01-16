99469a5fdf12d1e72b8e8f14e5aeea68ab5f9be5
Wen Ting Wang <chubberlisk@outlook.com>
2019-01-16 16:04:39 +0000

    refactor: remove #end tests and modify in line with DiceSet changes

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -11,7 +11,7 @@ describe Turn do
     expect(turn).not_to be_nil
   end
 
-  [:player, :accumulated_score, :end].each do |attribute|
+  [:player, :accumulated_score].each do |attribute|
     it { expect(turn).to respond_to(attribute) }
   end
 
@@ -31,91 +31,71 @@ describe Turn do
     end
   end
 
-  describe '#end' do
-    it 'is false when initialised' do
-      expect(turn.end).to be_falsey
-    end
-  end
-
   describe '#main' do
     it { expect(turn).to respond_to(:main) }
     
     context 'getting into the game' do
       it 'rolls 5 dice' do
-        dice_set = instance_double('DiceSet', values: [3, 3, 3, 4, 6], score: 300)
-        allow(dice_set).to receive(:num_of_available_dice).and_return(2)
-        allow(DiceSet).to receive(:new).and_return(dice_set)
+        allow(DiceSet).to receive(:roll).and_return(300, 2)
         allow(STDIN).to receive(:gets).and_return('n')
-        expect(dice_set).to receive(:roll).with(5)
+        expect(DiceSet).to receive(:roll).with(5)
         turn.main
       end
 
       it 'adds dice set score to accumulated score if >= 300' do
-        dice_set = instance_double('DiceSet', values: [3, 3, 3, 4, 6], score: rand(300..1000))
-        allow(dice_set).to receive(:num_of_available_dice).and_return(2)
-        allow(dice_set).to receive(:roll).and_return(5)
-        allow(DiceSet).to receive(:new).and_return(dice_set)
+        score = rand(300..1200)
+        allow(DiceSet).to receive(:roll).and_return(score, 2)
         allow(STDIN).to receive(:gets).and_return('n')
         turn.main
-        expect(turn.accumulated_score).to eq(dice_set.score)
+        expect(turn.accumulated_score).to eq(score)
       end
 
       it 'does not add dice set score to accumulated score if < 300' do
-        dice_set = instance_double('DiceSet', values: [3, 3, 3, 4, 6], score: rand(0...300))
-        allow(dice_set).to receive(:num_of_available_dice).and_return(2)
-        allow(dice_set).to receive(:roll).and_return(5)
-        allow(DiceSet).to receive(:new).and_return(dice_set)
+        allow(DiceSet).to receive(:roll).and_return(rand(0...300), 2)
         turn.main
         expect(turn.accumulated_score).to eq(0)
       end
 
-      it 'makes player in the game when dice set score is >= 300' do
-        dice_set = instance_double('DiceSet', values: [3, 3, 3, 4, 6], score: rand(300..1000))
-        allow(dice_set).to receive(:num_of_available_dice).and_return(2)
-        allow(dice_set).to receive(:roll).and_return(5)
-        allow(DiceSet).to receive(:new).and_return(dice_set)
+      it 'makes player in the game when dice set score is >= 300 and does not continue' do
+        allow(DiceSet).to receive(:roll).and_return(rand(300..1200), 2)
         allow(STDIN).to receive(:gets).and_return('n')
         turn.main
         expect(turn.player.in_the_game?).to be_truthy
       end
 
       it 'keeps player not in the game when dice set score is < 300' do
-        dice_set = instance_double('DiceSet', values: [3, 3, 3, 4, 6], score: rand(0...300))
-        allow(dice_set).to receive(:num_of_available_dice).and_return(2)
-        allow(dice_set).to receive(:roll).and_return(5)
-        allow(DiceSet).to receive(:new).and_return(dice_set)
+        allow(DiceSet).to receive(:roll).and_return(rand(0...300), 2)
         turn.main
         expect(turn.player.in_the_game?).to be_falsey
       end
 
       it 'allows player to continue rolling with available dice' do
-        dice_set = instance_double('DiceSet', values: [3, 3, 3, 4, 6], score: 300)
-        allow(dice_set).to receive(:num_of_available_dice).and_return(2)
-        allow(dice_set).to receive(:roll).and_return(5, 2)
-        allow(DiceSet).to receive(:new).and_return(dice_set)
-        allow(STDIN).to receive(:gets).and_return('y')
-        expect(dice_set).to receive(:roll).twice
+        allow(DiceSet).to receive(:roll).and_return(rand(300..1200), 2)
+        allow(STDIN).to receive(:gets).and_return('y', 'n')
+        expect(DiceSet).to receive(:roll).twice
+        turn.main
+      end
+
+      it 'allows player to continue rolling with available dice 3 times' do
+        allow(DiceSet).to receive(:roll).and_return(rand(300..1200), 2)
+        allow(STDIN).to receive(:gets).and_return('y', 'y', 'n')
+        expect(DiceSet).to receive(:roll).exactly(3).times
         turn.main
       end
 
       it 'allows player to not continue rolling' do
-        dice_set = instance_double('DiceSet', values: [3, 3, 3, 4, 6], score: 300)
-        allow(dice_set).to receive(:num_of_available_dice).and_return(2)
-        allow(dice_set).to receive(:roll).and_return(5)
-        allow(DiceSet).to receive(:new).and_return(dice_set)
+        allow(DiceSet).to receive(:roll).and_return(rand(300..1200), 2)
         allow(STDIN).to receive(:gets).and_return('n')
-        expect(dice_set).to receive(:roll).once
+        expect(DiceSet).to receive(:roll).once
         turn.main
       end
 
       it 'adds accumulated score to player points if player chooses to not continue' do
-        dice_set = instance_double('DiceSet', values: [3, 3, 3, 4, 6], score: 300)
-        allow(dice_set).to receive(:num_of_available_dice).and_return(2)
-        allow(dice_set).to receive(:roll).and_return(5)
-        allow(DiceSet).to receive(:new).and_return(dice_set)
+        score = rand(300..1200)
+        allow(DiceSet).to receive(:roll).and_return(score, 2)
         allow(STDIN).to receive(:gets).and_return('n')
         turn.main
-        expect(turn.player.points).to eq(300)
+        expect(turn.player.points).to eq(score)
       end
     end
   end
