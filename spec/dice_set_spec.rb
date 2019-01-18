eaea68e7f3ad028aad89fcd1525c2514ad8b1242
Wen Ting Wang <chubberlisk@outlook.com>
2019-01-18 14:42:28 +0000

    test: refactor for changes to DiceSet to a module

--- spec/dice_set_spec.rb
+++ spec/dice_set_spec.rb
@@ -14,100 +14,100 @@ describe DiceSet do
 
     it 'rolls different number of dice' do
       DiceSet.roll(3)
-      expect(DiceSet.instance_variable_get(:@values).size).to eq(3)
+      expect(DiceSet.class_variable_get(:@@values).size).to eq(3)
       DiceSet.roll(1)
-      expect(DiceSet.instance_variable_get(:@values).size).to eq(1)
+      expect(DiceSet.class_variable_get(:@@values).size).to eq(1)
     end
   end
 
   describe '.score' do
     it 'returns 0 when values is an empty list' do
-      DiceSet.instance_variable_set(:@values, [])
+      DiceSet.class_variable_set(:@@values, [])
       expect(DiceSet.score[:val]).to eq(0)
     end
 
     it 'returns 50 for a single roll of 5' do
-      DiceSet.instance_variable_set(:@values, [5])
+      DiceSet.class_variable_set(:@@values, [5])
       expect(DiceSet.score[:val]).to eq(50)
     end
 
     it 'returns 100 for a single roll of 1' do
-      DiceSet.instance_variable_set(:@values, [1])
+      DiceSet.class_variable_set(:@@values, [1])
       expect(DiceSet.score[:val]).to eq(100)
     end
 
     it 'returns sum of individual scores for multiple 1s and 5s' do
-      DiceSet.instance_variable_set(:@values, [1, 5, 5, 1])
+      DiceSet.class_variable_set(:@@values, [1, 5, 5, 1])
       expect(DiceSet.score[:val]).to eq(300)
     end
 
     it 'returns 0 for single 2s, 3s, 4s, and 6s' do
-      DiceSet.instance_variable_set(:@values, [2, 3, 4, 6])
+      DiceSet.class_variable_set(:@@values, [2, 3, 4, 6])
       expect(DiceSet.score[:val]).to eq(0)
     end
 
     it 'returns 1000 for a triple 1' do
-      DiceSet.instance_variable_set(:@values, [1, 1, 1])
+      DiceSet.class_variable_set(:@@values, [1, 1, 1])
       expect(DiceSet.score[:val]).to eq(1000)
     end
 
     it 'returns 100x for other triples' do
-      DiceSet.instance_variable_set(:@values, [2, 2, 2])
+      DiceSet.class_variable_set(:@@values, [2, 2, 2])
       expect(DiceSet.score[:val]).to eq(200)
-      DiceSet.instance_variable_set(:@values, [3, 3, 3])
+      DiceSet.class_variable_set(:@@values, [3, 3, 3])
       expect(DiceSet.score[:val]).to eq(300)
-      DiceSet.instance_variable_set(:@values, [4, 4, 4])
+      DiceSet.class_variable_set(:@@values, [4, 4, 4])
       expect(DiceSet.score[:val]).to eq(400)
-      DiceSet.instance_variable_set(:@values, [5, 5, 5])
+      DiceSet.class_variable_set(:@@values, [5, 5, 5])
       expect(DiceSet.score[:val]).to eq(500)
-      DiceSet.instance_variable_set(:@values, [6, 6, 6])
+      DiceSet.class_variable_set(:@@values, [6, 6, 6])
       expect(DiceSet.score[:val]).to eq(600)
     end
 
     it 'returns sum for mixed rolls' do
-      DiceSet.instance_variable_set(:@values, [2, 5, 2, 2, 3])
+      DiceSet.class_variable_set(:@@values, [2, 5, 2, 2, 3])
       expect(DiceSet.score[:val]).to eq(250)
-      DiceSet.instance_variable_set(:@values, [5, 5, 5, 5])
+      DiceSet.class_variable_set(:@@values, [5, 5, 5, 5])
       expect(DiceSet.score[:val]).to eq(550)
-      DiceSet.instance_variable_set(:@values, [1, 1, 1, 1])
+      DiceSet.class_variable_set(:@@values, [1, 1, 1, 1])
       expect(DiceSet.score[:val]).to eq(1100)
-      DiceSet.instance_variable_set(:@values, [1, 1, 1, 1, 1])
+      DiceSet.class_variable_set(:@@values, [1, 1, 1, 1, 1])
       expect(DiceSet.score[:val]).to eq(1200)
-      DiceSet.instance_variable_set(:@values, [1, 1, 1, 5, 1])
+      DiceSet.class_variable_set(:@@values, [1, 1, 1, 5, 1])
       expect(DiceSet.score[:val]).to eq(1150)
     end
 
-    it 'does not affect @values' do
+    it 'does not affect @@values' do
       values = [1, 1, 1, 1]
-      DiceSet.instance_variable_set(:@values, values)
+      DiceSet.class_variable_set(:@@values, values)
       DiceSet.score
-      expect(DiceSet.instance_variable_get(:@values)).to match_array(values)
+      expect(DiceSet.class_variable_get(:@@values)).to match_array(values)
     end
   end
 
   describe '.num_of_available_dice' do
     it 'returns 0 when all non-scoring dice' do
-      DiceSet.instance_variable_set(:@values, [2, 3, 4, 4, 6])
+      DiceSet.class_variable_set(:@@values, [2, 3, 4, 4, 6])
       expect(DiceSet.num_of_available_dice).to eq(0)
     end
 
     it 'returns 2 when 2 non-scoring dice' do
-      DiceSet.instance_variable_set(:@values, [5, 1, 3, 4, 1])
+      DiceSet.class_variable_set(:@@values, [5, 1, 3, 4, 1])
       expect(DiceSet.num_of_available_dice).to eq(2)
     end
 
     it 'returns 1 when 1 non-scoring dice' do
-      DiceSet.instance_variable_set(:@values, [1, 1, 1, 3, 1])
+      DiceSet.class_variable_set(:@@values, [1, 1, 1, 3, 1])
       expect(DiceSet.num_of_available_dice).to eq(1)
     end
 
     it 'returns 5 when all 5 scoring dice' do
-      DiceSet.instance_variable_set(:@values, [1, 1, 1, 1, 1])
+      DiceSet.class_variable_set(:@@values, [1, 1, 1, 1, 1])
       expect(DiceSet.num_of_available_dice).to eq(5)
     end
 
     it 'returns 5 when all 3 scoring dice' do
-      DiceSet.instance_variable_set(:@values, [1, 1, 1])
+      DiceSet.class_variable_set(:@@values, [1, 1, 1])
       expect(DiceSet.num_of_available_dice).to eq(5)
     end
   end
