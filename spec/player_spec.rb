5519a0f959ffc2dca7d859a31332d570d7102d05
Wen Ting Wang <chubberlisk@outlook.com>
2019-01-18 14:42:59 +0000

    test: add a few more tests for some methods and make neat

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -7,6 +7,14 @@ describe Player do
     expect(subject).not_to be_nil
   end
 
+  [:number, :points, :in_the_game?, :final_turn?].each do |get_method|
+    it { expect(subject).to respond_to(get_method) }
+  end
+
+  [:points=, :in_the_game=, :final_turn=].each do |set_method|
+    it { expect(subject).to respond_to(set_method) }
+  end
+
   describe '#number' do
     it 'returns an integer' do
       expect(subject.number).to be_kind_of(Integer)
@@ -18,7 +26,7 @@ describe Player do
       expect(subject.points).to eq(0)
     end
 
-    it 'returns 100 when changed' do
+    it 'returns 100 when set to 100' do
       subject.points = 100
       expect(subject.points).to eq(100)
     end
@@ -26,6 +34,11 @@ describe Player do
 
   describe '#in_the_game?' do
     it 'returns false when initialised' do
+      expect(subject.in_the_game?).to be_falsey
+    end
+
+    it 'returns false when their points is less than 300' do
+      subject.points = rand(0...300)
       expect(subject.in_the_game?).to be_falsey
     end
 
@@ -40,10 +53,13 @@ describe Player do
     end
   end
 
-  it { expect(subject).to respond_to(:in_the_game=) }
-
   describe '#final_turn?' do
     it 'returns false when initialised' do
+      expect(subject.final_turn?).to be_falsey
+    end
+
+    it 'returns false when their points is less than 3000' do
+      subject.points = rand(0...3000)
       expect(subject.final_turn?).to be_falsey
     end
 
@@ -57,6 +73,4 @@ describe Player do
       expect(subject.final_turn?).to be_truthy
     end
   end
-
-  it { expect(subject).to respond_to(:final_turn=) }
 end
